fa9c8e1c4008fff9802602dbeb0a3998aa39439f
w-barros <75806115+w-barros@users.noreply.github.com>
2022-09-22 15:04:34 -0300

    minor feature added: check for non-unique ports

--- validatePorts.ts
+++ validatePorts.ts
@@ -1,82 +1,92 @@
+function isValidPort(port: string): boolean {
+  return (parseInt(port) > 0 && parseInt(port) <= 65535)
+}
 
-function isValidPort(port:string): boolean {
-    return (parseInt(port) > 0 && parseInt(port) <= 65535)
+function isValidPortRange(range: string): boolean {
+  if (range.split("-").length != 2) {
+    return false
   }
 
-function isValidPortRange(range:string): boolean {
-    if (range.split("-").length != 2) {
-      return false
-    }
-
-    let rangeStart = range.split("-")[0]
-    let rangeEnd = range.split("-")[1]
+  let rangeStart = range.split("-")[0]
+  let rangeEnd = range.split("-")[1]
 
 
-    // valid port check
-    if (!isValidPort(rangeStart) || !isValidPort(rangeEnd)) {
-      return false
-    }
+  // Valid port check on both ends
+  if (!isValidPort(rangeStart) || !isValidPort(rangeEnd)) {
+    return false
+  }
 
-    // valid range check
-    if (rangeStart >= rangeEnd) {
-      return false
-    }
+  // Valid range check - start must be less than end
+  if (rangeStart >= rangeEnd) {
+    return false
+  }
 
+  return true
+}
+
+function validatePorts(userInput: string): boolean {
+
+  // Invalid combination check
+  if (userInput.includes("- ") || userInput.includes(" -") || userInput.endsWith("-") || userInput.trim().length === 0) {
+    return false
+  }
+
+  // Input is exactly "any" case - is allowed
+  if (userInput === "any") {
     return true
   }
 
-function validatePorts(ports: string): boolean {
+  // Check if contains text or is empty string - not allowed
+  let letters = /[a-zA-Z]/
+  if (letters.test(userInput) || userInput.trim().length === 0) {
+    return false
+  }
 
-    // invalid combination check
-    if (ports.includes("- ") || ports.includes(" -") || ports.endsWith("-") || ports.trim().length === 0) {
-        return false
-    }
+  // Input contains multiple ports/ranges case
+  if (userInput.includes(" ")) {
 
-    // "any" case
-    if (ports === "any") {
-      return true
-    }
+    let isInvalidInput;
+    let currentPortList: string[] = [];
+    let inputAsList = userInput.split(" ")
 
-    // check if contains text
-    let letters = /[a-zA-Z]/
-    if (letters.test(ports) || ports.trim().length === 0) {
-      return false
-    }
+    inputAsList.forEach(element => {
 
-    //multiple ports case
-    if (ports.includes(" ")) {
-      
-      let portList = ports.split(" ")
-
-      portList.forEach(port => {
-
-        // port range case
-        if (port.includes("-")) {
-          if (!isValidPortRange(port)) {
-            return false
-          }
-        }
-        // single port case
-        else if (!isValidPort(port)) {
-          return false
-        }
-
-        return true
-
-      });
-    } else {
-      // single range case
-      if (ports.includes("-")) {
-        if (!isValidPortRange(ports)) {
-          return false
+      // Element is a port range case - check if it is a valid range and not repeated
+      if (element.includes("-")) {
+        if (!isValidPortRange(element)) {
+          isInvalidInput = true;
+        } else {
+          currentPortList.includes(element) ? isInvalidInput = true : currentPortList.push(element)
         }
       }
 
-      // single port case
-      return isValidPort(ports)
+      // Element is a single port case
+      else if (!isValidPort(element)) {
+        isInvalidInput = true;
+      } else {
+        currentPortList.includes(element) ? isInvalidInput = true : currentPortList.push(element)
+      }
+      return true
+    });
+    if (isInvalidInput) {
+      return false
     }
-    return true
+
+    // Input is a single port or single range case (no " ")
+  } else {
+
+    // Single range case
+    if (userInput.includes("-")) {
+      if (!isValidPortRange(userInput)) {
+        return false
+      }
+    }
+
+    // Single port case
+    return isValidPort(userInput)
   }
+  return true
+}
 
 export {validatePorts}
 export {isValidPort}
